297e11687bab5b1a03fac67cc85ba147364d03e5
Dania <a01567218@tec.mx>
2025-04-26 18:19:53 -0600

    sidebar medio bien

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,62 @@
 // src/components/Sidebar.js
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { FaHome, FaList, FaPlusCircle, FaEdit, FaTrash, FaSignOutAlt, FaFileUpload,FaAddressBook, FaFileAlt } from 'react-icons/fa';
 import './Sidebar.css';
+import { IoPersonAdd } from "react-icons/io5";
+import { HiUserAdd, HiDocumentAdd, HiClipboardList } from "react-icons/hi";
+import { HiHome } from "react-icons/hi2";
+
 
 function Sidebar() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => {
     navigate('/login');
-    localStorage.removeItem('user'); // opcional, si usas localStorage
+    localStorage.removeItem('user');
   };
 
   return (
-    <div id="sidebar">  
-    <img src="/img/logomonos.png" alt="Logo ECOA" className="logo-jiji" />
-      <ul>
-        <li><Link to="/menu">Inicio</Link></li>
-        <li><Link to="/listapersonajes">Lista de personajes</Link></li>
-        <li><Link to="/crearpersonaje">Crear personaje</Link></li>
-        <li><Link to="/actualizarpersonaje">Actualizar personaje</Link></li>
-        <li><Link to="/eliminarpersonaje">Eliminar personaje</Link></li>
-        <li><button onClick={handleLogout}>Cerrar sesión</button></li>
+    <div id="sidebar" className="slide-in">
+      <img src="/img/logomonos.png" alt="Logo ECOA" className="logo-jiji" />
+      <ul className="space-y-2 mt-6">
+        <li>
+          <Link to="/menu" className={`sidebar-link ${location.pathname === '/menu' ? 'active' : ''}`}>
+            <HiHome className="home-icon" />
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link to="/listapersonajes" className={`sidebar-link ${location.pathname === '/listapersonajes' ? 'active' : ''}`}>
+            <HiClipboardList className="sidebar-icon" />
+            Lista de personajes
+          </Link>
+        </li>
+        <li>
+          <Link to="/crearpersonaje" className={`sidebar-link ${location.pathname === '/crearpersonaje' ? 'active' : ''}`}>
+            <HiDocumentAdd className="add-icon" />
+            Crear personaje
+          </Link>
+        </li>
+        <li>
+          <Link to="/actualizarpersonaje" className={`sidebar-link ${location.pathname === '/actualizarpersonaje' ? 'active' : ''}`}>
+            <HiUserAdd className="sidebar-icon" />
+            Actualizar personaje
+          </Link>
+        </li>
+        <li>
+          <Link to="/eliminarpersonaje" className={`sidebar-link ${location.pathname === '/eliminarpersonaje' ? 'active' : ''}`}>
+            <FaTrash className="sidebar-icon" />
+            Eliminar personaje
+          </Link>
+        </li>
+        <li>
+          <button onClick={handleLogout} className="sidebar-link">
+            <FaSignOutAlt className="sidebar-icon" />
+            Cerrar sesión
+          </button>
+        </li>
       </ul>
     </div>
   );
